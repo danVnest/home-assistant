864d713678c1ddada737a0bcf9ab3135b53c632b
Daniel Vogelnest <daniel.vogelnest@me.com>
2024-01-31 17:48:27 +1100

    Monitor phone batteries

--- appdaemon/apps/control.py
+++ appdaemon/apps/control.py
@@ -59,6 +59,8 @@ class Control(app.App):
         self.listen_event(self.__button, "zwave_js_value_notification")
         self.listen_event(self.__ifttt, "ifttt_webhook_received")
         for battery in (
+            "daniels_phone",
+            "rachels_phone",
             "front_door_camera",
             "back_door_camera",
             "entryway_camera",
